3d93a0e416783f37e3ba82a95f551628271f43e6
JiangWeiGitHub <wei.jiang@winsuntech.cn>
2017-05-15 16:11:51 +0800

    modified:   node/lib/window.js 	modified:   package.json

--- node/lib/window.js
+++ node/lib/window.js
@@ -36,7 +36,7 @@ const initMainWindow = () => {
 	})
 
   // debug mode
-  _mainWindow.webContents.openDevTools()
+  //_mainWindow.webContents.openDevTools()
   // _mainWindow.maximize()
 
   // _mainWindow.maximize()
