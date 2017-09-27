af832cdc081e2dd60835a25113ef8c13bb4a4de3
lixinwei <xinwei.li@winsuntech.cn>
2017-09-27 16:24:15 +0800

    test

--- src/common/motion.js
+++
@@ -1,7 +0,0 @@
-
-// see material design motion
-
-export const sharpCurveDuration = 150
-export const sharpCurveDelay = sharpCurveDuration * 2
-export const sharpCurve = (prop) => `${prop} ${sharpCurveDuration}ms cubic-bezier(0.4, 0.0, 0.6, 1)`
-
